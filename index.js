1d621dfecf788b439d32a1cebbd7b7c516be4895
giTomas <tomas.kosegi@gmail.com>
2017-01-14 01:21:29 +0100

    lint config dir structure

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/app';
-import './assets/css/reset.css';
+import App from './app';
+import './spp/assets/css/reset.css';
 
 const renderApp = () =>
     render(
