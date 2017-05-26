acbbc58232badd8e824bd8a1e1b1c9d656ae0d2e
MAIN\SK040122 <Tomas.Kosegi@anritsu.com>
2017-05-26 09:33:22 +0200

    update webpack

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,45 @@
 import React from 'react';
-import R from 'ramda';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from 'react-router-dom';
+// import R from 'ramda';
 import Hello from './components/hello';
 // import styled from 'styled-components';
 
-const App = () =>
-  <Hello>Hello New App!!</Hello>;
+// const App = () =>
+//   <Hello>Hello New App!!</Hello>;
+
+const Hi = () => <Hello>Hello New App!!</Hello>;
+const He = () => <Hello>Hello User!!</Hello>;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+    };
+  }
+
+  render() {
+    // return <Hello>Hello New App!!</Hello>;
+    return (
+      <Router>
+        <div>
+          <Link to="/">Hello</Link>
+          <Link to="/about">About</Link>
+          <hr />
+
+
+          <Route exact path="/" component={Hi} />
+          <Route path="/about" component={He} />
+
+          <div>Footer</div>
+        </div>
+      </Router>
+    );
+  }
+}
 
 export default App;
